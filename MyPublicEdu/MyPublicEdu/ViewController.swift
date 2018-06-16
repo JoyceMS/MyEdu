2318e4c941605e0716bafc63f95a9cae12b18ee9
Joyce306 <x304306@163.com>
2018-06-16 11:21:52 +0800

    "init"

---
+++ MyPublicEdu/MyPublicEdu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPublicEdu
+//
+//  Created by xuyou on 2018/6/16.
+//  Copyright © 2018年 xuyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
